14e301f89400ab12444f40fa41869f23e37cffeb
Karina Pietra <karinapietra95@gmail.com>
2018-10-13 17:46:14 -0400

    hid api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import API_KEY from './config.js'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -8,7 +9,7 @@ class App extends Component {
     center: {
       lat: 59.95,
       lng: 30.33
-      // have to change latitude and longitude 
+      // have to change latitude and longitude
     },
     zoom: 11
   };
@@ -18,7 +19,7 @@ class App extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key:'AIzaSyBbgCl-eZgttfQnvBzTkU0v9FGEmqKMZU0' }}
+          bootstrapURLKeys={{ key:`${API_KEY}` }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
