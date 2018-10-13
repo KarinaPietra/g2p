40de905b4b0a5bc97426c27a4c5196da248f9616
Karina Pietra <karinapietra95@gmail.com>
2018-10-13 17:40:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+      // have to change latitude and longitude 
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key:'AIzaSyBbgCl-eZgttfQnvBzTkU0v9FGEmqKMZU0' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/Data.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Data extends Component{
+  constructor(){
+    super();
+    this.state = { location:[]}
+  }
+}
+
+componentDidMount() {
+  axios.get('https://www.refugerestrooms.org/api/v1/restrooms/search.json?query=new%20york&per_page=100')
+  .then(function (response) {
+    const data = response.data
+    new_york_locations = data.map((location)=>{
+      if (location.city == "New York"){
+        return {latitude: location.latitde, longitude: location.longitude, city: location.city}
+      }
+    })
+    new_york_locations = new_york_locations.filter(function(el) { return el; });
+    console.log(new_york_locations)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+render(){
+  return (
+    <ul>
+    {this.state.location.map(location =>
+      <li>
+        // {specify what we want the list to be}
+      </li>
+    )}
+    </ul>
+    )
+}
+
+export default Data
