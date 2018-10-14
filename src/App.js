665a485b36423fba6dcaeae5b93e4a51185cda9a
Karina Pietra <karinapietra95@gmail.com>
2018-10-14 16:56:48 -0400

    map-box

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import API_KEY from './config.js'
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import logo from './logo.svg';
+import Map from './components/map.js'
 
 class App extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-      // have to change latitude and longitude
-    },
-    zoom: 11
-  };
-
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:`${API_KEY}` }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
+      <div className="App">
+        <br/>
+        <Map/>
       </div>
-    );
+    )
   }
 }
-
 
 export default App;
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
 class Data extends Component{
   constructor(){
     super();
     this.state = { location:[]}
   }
-}
 
 componentDidMount() {
   axios.get('https://www.refugerestrooms.org/api/v1/restrooms/search.json?query=new%20york&per_page=100')
   .then(function (response) {
     const data = response.data
-    new_york_locations = data.map((location)=>{
-      if (location.city == "New York"){
-        return {latitude: location.latitde, longitude: location.longitude, city: location.city}
+    let new_york_locations = data.map((location)=>{
+      if (location.city === "New York"){
+        return {latitude: location.latitude, longitude: location.longitude, city: location.city}
+      }
+      else{
+        return null
       }
     })
     new_york_locations = new_york_locations.filter(function(el) { return el; });
@@ -24,17 +25,16 @@ componentDidMount() {
     console.log(error);
   });
 }
-
 render(){
   return (
     <ul>
     {this.state.location.map(location =>
       <li>
-        // {specify what we want the list to be}
+        {/* // {specify what we want the list to be} */}
       </li>
     )}
     </ul>
     )
+  }
 }
-
 export default Data
