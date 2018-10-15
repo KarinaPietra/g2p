660fbdb20e9b831192cf1a05e96ff97bebf2d85e
Karina Pietra <karinapietra95@gmail.com>
2018-10-15 17:32:24 -0400

    added Miami

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,25 +2,41 @@ import React, { Component } from 'react';
 import axios from 'axios';
 // import Map from './Map.js'
 //new code
-export const Data = () => {
+export const Data = async () => {
   let new_york_locations = []
-  return axios.get('https://www.refugerestrooms.org/api/v1/restrooms/search.json?query=new%20york&per_page=100')
-  .then((response) => {
-    const data = response.data
-     new_york_locations = data.map((location)=>{
-      if (location.city === "New York"){
-        return location
-        // return {latitude: location.latitude, longitude: location.longitude, city: location.city, name: location.name}
-      }
-      else{
-        return null
-      }
-    })
-    new_york_locations = new_york_locations.filter((el) =>   !!el )
-    return new_york_locations
+  let miami_locations = []
+  let nyReq = await axios.get('https://www.refugerestrooms.org/api/v1/restrooms/search.json?query=new%20york&per_page=100')
+  let miaReq = await axios.get('https://www.refugerestrooms.org/api/v1/restrooms/search.json?query=miami&per_page=100')
+  // .then((response) => {
+  const dataNy = nyReq.data
+  const dataMia = miaReq.data
+  new_york_locations = dataNy.map((location)=>{
+    if (location.city === "New York"){
+      return location
+      // return {latitude: location.latitude, longitude: location.longitude, city: location.city, name: location.name}
+    }
+    else{
+      return null
+    }
   })
-  .catch(function (error) {
-    console.log(error);
-  });
+
+  new_york_locations = new_york_locations.filter((el) =>   !!el )
+
+  miami_locations = dataMia.map((location)=>{
+    if (location.city === "Miami"){
+      return location
+      // return {latitude: location.latitude, longitude: location.longitude, city: location.city, name: location.name}
+    }
+    else{
+      return null
+    }
+  })
+
+  miami_locations = miami_locations.filter((el) =>   !!el )
+
+  return [...new_york_locations,...miami_locations]
+
 }
+
+
 export default Data
