ca11c2dbaa5539dfb138726e76b51e63dfddbf7d
Karina Pietra <karinapietra95@gmail.com>
2018-10-15 11:58:12 -0400

    fixed capital letter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Map from './components/map.js'
+import Map from './components/Map.js'
 
 class App extends Component {
   render() {
